bf115fd6c74cdcd02c02bb29af1e57330ba4e5e1
Matthew McGuff <matthewmcguff85@gmail.com>
2017-09-18 22:45:26 -0600

    Initial Commit

---
+++ index.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+ <title>02 Teach : Team Activity</title>
+ <link rel="stylesheet" type="text/css" href="style.css">
+ <script src="javascript.js"></script>
+
+</head>
+<body>
+<?php 
+
+/*
+Create an HTML page with a head element that provides a title for your page, and links to external CSS and JavaScript files.
+
+On the HTML page, create 3 divs and a button labeled "Click Me".
+
+Add classes and/or ids to your divs, then use the CSS to set their font-size and background color. Add function in your JavaScript file to alert the text "Clicked!", and have your button call this function when it is clicked.
+
+Use CSS to make it so that hovering over any of the 3 divs causes the text to temporarily become bold.
+
+Make it so that the user can specify custom colors for the first div. To accomplish this, add a text box and another button labeled "Change color". Write a JavaScript function that gets invoked by clicking this button that gets the text from the textbox and sets the color of the first div.
+*/
+
+
+
+
+
+
+
+?> 
+</body>
+</html>
