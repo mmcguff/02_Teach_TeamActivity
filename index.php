26779f853549a256300840ec339f74702b7ccffd
Matthew McGuff <matthewmcguff85@gmail.com>
2017-09-18 22:51:46 -0600

    Created 3 divs and the button.

--- index.php
+++ index.php
@@ -10,9 +10,9 @@
 
 /*
 Create an HTML page with a head element that provides a title for your page, and links to external CSS and JavaScript files.
-
+done
 On the HTML page, create 3 divs and a button labeled "Click Me".
-
+done
 Add classes and/or ids to your divs, then use the CSS to set their font-size and background color. Add function in your JavaScript file to alert the text "Clicked!", and have your button call this function when it is clicked.
 
 Use CSS to make it so that hovering over any of the 3 divs causes the text to temporarily become bold.
@@ -20,8 +20,10 @@ Use CSS to make it so that hovering over any of the 3 divs causes the text to te
 Make it so that the user can specify custom colors for the first div. To accomplish this, add a text box and another button labeled "Change color". Write a JavaScript function that gets invoked by clicking this button that gets the text from the textbox and sets the color of the first div.
 */
 
-
-
+echo "<div id='1'>div 1</div>";
+echo "<div id='2'>div 2</div>";
+echo "<div id='3'>div 3</div>";
+echo "<button>Click Me</button>";
 
 
 
